36b42102cff6c6597d0fe9567f4617db85993783
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-03-17 13:59:48 -0700

    function cont lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(string = "roller-skate"){
+    return `This Saturday, I want to ${string}!`
+}
+saturdayFun()
+
+  // defined using far arrow
+
+  let mondayWork = (string = "go to the office") =>{ 
+    return `This Monday, I will ${string}.`
+  }
+  mondayWork();
+
+  //nested functions
+
+  function wrapAdjective(string1 = "*") {
+    return function (string = "special"){
+      return `You are ${string1}${string}${string1}!`
+    }
+  }
+  console.log(wrapAdjective())
